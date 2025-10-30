0d65481b8951789a37d6167057d0004e3e40965c
SimonKane <simon.kaneborn@chasacademy.se>
2025-10-30 14:14:07 +0100

    setup for mongodb

---
+++ backend/src/config/db.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+  if (!process.env.MONGODB_URI) throw new Error("Missing MONGODB_URI");
+  await mongoose.connect(process.env.MONGODB_URI, {
+    dbName: "daybouk",
+  });
+  console.log("MongoDB connected");
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+await connectDB();
 
 app.get("/", (req, res) => {
   res.send("Node.js and Express.js with TypeScript");
