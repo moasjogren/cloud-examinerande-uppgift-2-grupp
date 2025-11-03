0ff50c593c6a5e1b9d2008a7182dca5ceb5916e8
Moa Sjögren <moasjgren@gmail.com>
2025-11-03 19:36:31 +0100

    + entryModel

--- backend/src/models/entryModel.ts
+++ backend/src/models/entryModel.ts
@@ -1,3 +1,16 @@
 import mongoose, { Schema, InferSchemaType, Types } from "mongoose";
 
-const entrySchema = new Schema({});
+const entrySchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: { type: [String], default: [] },
+    userId: { type: Types.ObjectId, required: true },
+  },
+  { timestamps: true, collection: "entries" }
+);
+
+export type EntryType = InferSchemaType<typeof entrySchema> & {
+  _id: Types.ObjectId;
+};
+export const Entry = mongoose.model("Entry", entrySchema);
