48626dac32a3053e4962d4deaee19ca3edf3fdbc
Moa Sjögren <moasjgren@gmail.com>
2025-11-05 17:55:06 +0100

    + middleware som validerar id för user och entry

--- backend/src/routes/entryRoutes.ts
+++ backend/src/routes/entryRoutes.ts
@@ -8,13 +8,14 @@ import {
   updateEntry,
 } from "../controllers/entryController";
 
+import { validateEntryId } from "../middleware/validateId";
+
 const router = Router();
 
 router.get("/entries", getAllEntries);
 router.post("/entries", createEntry);
-router.get("/entries/:id", getEntryById);
-router.patch("/entries/:id", updateEntry);
-router.delete("/entries/:id", deleteEntry);
-
+router.get("/entries/:id", validateEntryId, getEntryById);
+router.patch("/entries/:id", validateEntryId, updateEntry);
+router.delete("/entries/:id", validateEntryId, deleteEntry);
 
 export default router;
