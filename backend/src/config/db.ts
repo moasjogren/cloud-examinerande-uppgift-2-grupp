956773ca3a2773844bbef98375b73ff96d0cc7a6
Moa Sjögren <moasjgren@gmail.com>
2025-11-06 10:28:26 +0100

    + entryController - uppdaterad index.ts med routes

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export async function connectDB() {
   if (!process.env.MONGODB_URI) throw new Error("Missing MONGODB_URI");
   await mongoose.connect(process.env.MONGODB_URI, {
-    dbName: "daybouk",
+    dbName: "Daybouk",
   });
   console.log("MongoDB connected");
 }
