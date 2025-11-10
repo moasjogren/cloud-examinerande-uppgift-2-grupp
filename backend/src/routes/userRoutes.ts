138427d076d55d1b203ecf37eac0397253e5986a
FelixOS <puffis.stenersjo@gmail.com>
2025-11-10 11:08:58 +0100

    added better flow for login logout, also added JWT and logic for it so it doesnt save in localstorage

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -6,18 +6,21 @@ import {
   deleteUser,
   updateUser,
   loginUser,
+  logoutUser
 } from "../controllers/userController";
+import authUser from '../middleware/authentication';
 
 import { validateUserId } from "../middleware/validateId";
 import { validateUser, validateUpdatedUser } from "../middleware/validateUser";
 
 const router = express.Router();
 
-router.get("/users", getAllUsers);
+router.get("/users", authUser, getAllUsers);
 router.post("/users/login", loginUser);
-router.post("/users", validateUser, createUser);
-router.get("/users/:id", validateUserId, getUserById);
-router.patch("/users/:id", validateUserId, validateUpdatedUser, updateUser);
-router.delete("/users/:id", validateUserId, deleteUser);
+router.post("/users/logout", logoutUser);
+router.post("/users",  validateUser, createUser);
+router.get("/users/:id", authUser, validateUserId, getUserById);
+router.patch("/users/:id", authUser, validateUserId, validateUpdatedUser, updateUser);
+router.delete("/users/:id", authUser, validateUserId, deleteUser);
 
 export default router;
